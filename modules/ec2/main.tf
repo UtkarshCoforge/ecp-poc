4fb10fee322c289da5014a47ef0e77fdf3fe9953
utkarshCoforge <utkarsh.2.s@coforge.com>
2025-01-19 12:33:05 +0530

    edit main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -4,34 +4,12 @@ provider "aws" {
 
 
 
-# Create a Security Group
-resource "aws_security_group" "ec2_sg" {
-  name_prefix = "ec2-sg-"
-  vpc_id      = aws_vpc.main.vpc_id
-
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # No inbound access allowed
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"] # Allow all outbound traffic
-  }
-   depends_on = [aws_instance.web]
-}
-
 resource "aws_instance" "web" {
   ami             = var.ami_id
   instance_type   = var.instance_type
   subnet_id       = var.subnet_id
   iam_instance_profile = var.iam_instance_profile
-  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  # vpc_security_group_ids = var.security_group_id
   tags = {
     Name = var.instance_name
   }
