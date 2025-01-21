f20a8db4769efd0bebabeda5f1254e6b05141557
utkarshCoforge <utkarsh.2.s@coforge.com>
2025-01-21 23:26:48 +0530

    infra changed

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -104,13 +104,19 @@ resource "aws_security_group" "ec2_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # No inbound access allowed
+    cidr_blocks = ["10.0.0.0/16"] # No inbound access allowed
   }
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # No inbound access allowed
+    cidr_blocks = ["10.0.0.0/16"] # No inbound access allowed
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"] # No inbound access allowed
   }
 
   egress {
