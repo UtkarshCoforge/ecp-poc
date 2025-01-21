41ef7e19cc57285ad6fb23e7d2a926f671899d8c
utkarshCoforge <utkarsh.2.s@coforge.com>
2025-01-21 23:36:42 +0530

    infra changed

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -8,6 +8,7 @@ resource "aws_instance" "web" {
   ami             = var.ami_id
   instance_type   = var.instance_type
   subnet_id       = var.subnet_id
+  security_groups = var.security_group_id
   iam_instance_profile = var.iam_instance_profile
   key_name        = "test_US"
   # vpc_security_group_ids = var.security_group_id
