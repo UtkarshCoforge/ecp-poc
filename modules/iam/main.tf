bce44a1f2a89ca88556907c17b455bec28cffdb7
utkarshCoforge <utkarsh.2.s@coforge.com>
2025-01-21 19:45:01 +0530

    ec2 changed

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -19,63 +19,64 @@ resource "aws_iam_role" "ssm_role" {
   })
 }
 
-resource "aws_iam_policy" "ssm_policy" {
-  name        = "SSMPolicy"
-  description = "Allow EC2 instances to communicate with Systems Manager"
-  policy      = jsonencode(
-    {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "ssm:DescribeAssociation",
-                "ssm:GetDeployablePatchSnapshotForInstance",
-                "ssm:GetDocument",
-                "ssm:DescribeDocument",
-                "ssm:GetManifest",
-                "ssm:GetParameter",
-                "ssm:GetParameters",
-                "ssm:ListAssociations",
-                "ssm:ListInstanceAssociations",
-                "ssm:PutInventory",
-                "ssm:PutComplianceItems",
-                "ssm:PutConfigurePackageResult",
-                "ssm:UpdateAssociationStatus",
-                "ssm:UpdateInstanceAssociationStatus",
-                "ssm:UpdateInstanceInformation"
-            ],
-            "Resource": "*"
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "ssmmessages:CreateControlChannel",
-                "ssmmessages:CreateDataChannel",
-                "ssmmessages:OpenControlChannel",
-                "ssmmessages:OpenDataChannel"
-            ],
-            "Resource": "*"
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "ec2messages:AcknowledgeMessage",
-                "ec2messages:DeleteMessage",
-                "ec2messages:FailMessage",
-                "ec2messages:GetEndpoint",
-                "ec2messages:GetMessages",
-                "ec2messages:SendReply"
-            ],
-            "Resource": "*"
-        }
-    ]
-})
-}
+# resource "aws_iam_policy" "ssm_policy" {
+#   name        = "SSMPolicy"
+#   description = "Allow EC2 instances to communicate with Systems Manager"
+#   policy      = jsonencode(
+#     {
+#     "Version": "2012-10-17",
+#     "Statement": [
+#         {
+#             "Effect": "Allow",
+#             "Action": [
+#                 "ssm:DescribeAssociation",
+#                 "ssm:GetDeployablePatchSnapshotForInstance",
+#                 "ssm:GetDocument",
+#                 "ssm:DescribeDocument",
+#                 "ssm:GetManifest",
+#                 "ssm:GetParameter",
+#                 "ssm:GetParameters",
+#                 "ssm:ListAssociations",
+#                 "ssm:ListInstanceAssociations",
+#                 "ssm:PutInventory",
+#                 "ssm:PutComplianceItems",
+#                 "ssm:PutConfigurePackageResult",
+#                 "ssm:UpdateAssociationStatus",
+#                 "ssm:UpdateInstanceAssociationStatus",
+#                 "ssm:UpdateInstanceInformation"
+#             ],
+#             "Resource": "*"
+#         },
+#         {
+#             "Effect": "Allow",
+#             "Action": [
+#                 "ssmmessages:CreateControlChannel",
+#                 "ssmmessages:CreateDataChannel",
+#                 "ssmmessages:OpenControlChannel",
+#                 "ssmmessages:OpenDataChannel"
+#             ],
+#             "Resource": "*"
+#         },
+#         {
+#             "Effect": "Allow",
+#             "Action": [
+#                 "ec2messages:AcknowledgeMessage",
+#                 "ec2messages:DeleteMessage",
+#                 "ec2messages:FailMessage",
+#                 "ec2messages:GetEndpoint",
+#                 "ec2messages:GetMessages",
+#                 "ec2messages:SendReply"
+#             ],
+#             "Resource": "*"
+#         }
+#     ]
+# })
+# }
 
 resource "aws_iam_role_policy_attachment" "ssm_attach" {
   role       = aws_iam_role.ssm_role.name
-  policy_arn = aws_iam_policy.ssm_policy.arn
+#   policy_arn = aws_iam_policy.ssm_policy.arn
+policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
 resource "aws_iam_instance_profile" "ec2_profile" {
