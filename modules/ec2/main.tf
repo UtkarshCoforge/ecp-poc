366df1e400ab05acd5359d8d52f8497f84887447
utkarshCoforge <utkarsh.2.s@coforge.com>
2025-01-21 22:02:43 +0530

    infra changed

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -16,8 +16,8 @@ resource "aws_instance" "web" {
 
     user_data = <<-EOT
     #!/bin/bash
-    sudo yum update -y
-    sudo yum install -y amazon-ssm-agent
+    cd /tmp
+    sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
     sudo systemctl enable amazon-ssm-agent
     sudo systemctl start amazon-ssm-agent
   EOT
