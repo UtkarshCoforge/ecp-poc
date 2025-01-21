50e64cc1afa96cd07e656a6c588a7a78861ef791
utkarshCoforge <utkarsh.2.s@coforge.com>
2025-01-21 19:52:04 +0530

    ec2 changed

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -103,13 +103,13 @@ resource "aws_security_group" "ec2_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # No inbound access allowed
+    cidr_blocks = ["10.0.0.0/16"] # No inbound access allowed
   }
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # No inbound access allowed
+    cidr_blocks = ["10.0.0.0/16"] # No inbound access allowed
   }
 
   egress {
