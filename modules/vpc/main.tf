7795fcf36b7498ac092f9a0422da9304ca651dd7
utkarshCoforge <utkarsh.2.s@coforge.com>
2025-01-22 09:39:38 +0530

    infra changed

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -56,6 +56,24 @@ resource "aws_nat_gateway" "nat" {
   }
 }
 
+# Create a Route Table for the Public Subnet
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    gateway_id     = aws_internet_gateway.gw.id
+  }
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+# Associate Subnet with Route Table
+resource "aws_route_table_association" "public_rt_assoc" {
+  subnet_id      = aws_subnet.subnet-b.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
 # Create a Route Table for the Private Subnet
 resource "aws_route_table" "private_rt" {
   vpc_id = aws_vpc.main.id
