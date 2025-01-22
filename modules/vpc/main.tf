3b350d024e81dc0e6db04cb829660b2dce0050e4
utkarshCoforge <utkarsh.2.s@coforge.com>
2025-01-22 09:12:41 +0530

    infra changed

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -58,7 +58,7 @@ resource "aws_nat_gateway" "nat" {
 # Create a Route Table for the Private Subnet
 resource "aws_route_table" "private_rt" {
   vpc_id = aws_vpc.main.id
-  route = {
+  route {
     cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat.id
   }
