20d091c1ad28ce7109012fb76641abfffba8a5d4
utkarshCoforge <utkarsh.2.s@coforge.com>
2025-01-22 09:03:50 +0530

    infra changed

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ module "vpc" {
   cidr_block      = "10.0.0.0/16"
   vpc_name        = "my-vpc"
   subnet_a_cidr   = "10.0.1.0/24"
+  subnet_b_cidr   = "10.0.2.0/24"
   az_a            = "us-east-1a"
 }
 
